d69a7b60f37bfb41131e534ac11f3d821a4b7af6
programmer-SS <eiays930@icloud.com>
2024-05-05 16:18:50 +0900

    seventeen

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ * 17 の倍数である場合 true を返す
+ * @param {number} num
+ * @returns {boolean}
+ */
+function isMultipleOfSeventeen(num) {
+  return num % 17 === 0
+}
 
 module.exports = {
-}
+  isMultipleOfSeventeen
+};
